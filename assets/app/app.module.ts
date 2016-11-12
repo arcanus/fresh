44f5b346e1295cc9a990f6a83ed47913d6e0f54f
ArcanuS <kitarofana@gmail.com>
2016-11-12 20:52:49 -0300

    Se creó el router y el HomeComponent

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        HomeComponent
     ],
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+          { path: '', component: HomeComponent}
+        ])
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
