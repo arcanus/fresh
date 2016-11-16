d8fe5cdb8d05f3eade849c44d7f23c34567eab61
ArcanuS <kitarofana@gmail.com>
2016-11-16 11:29:00 -0300

    Se creó el componente users y se actualizaron las rutas

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -5,17 +5,20 @@ import { RouterModule }   from '@angular/router';
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        HomeComponent
+        HomeComponent,
+        UsersComponent
     ],
     imports: [
         BrowserModule,
         RouterModule.forRoot([
-          { path: '', component: HomeComponent}
+          { path: '', component: HomeComponent},
+          { path: 'users', component: UsersComponent}
         ])
     ],
     bootstrap: [AppComponent]
