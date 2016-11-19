72310f02ea4fd9ddcb0c9cdc0e62fd9084a74943
ArcanuS <kitarofana@gmail.com>
2016-11-18 23:07:22 -0300

    Se arregló el tema del tipo de dato chai

--- assets/app/components/clientes/clientes.service.ts
+++ assets/app/components/clientes/clientes.service.ts
@@ -1,8 +1,7 @@
 import { Injectable }     from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-//import { Observable }     from 'rxjs/Observable';
-//import 'rxjs/operator/map';
-//import 'rxjs/Rx';
+import 'rxjs/Rx';
+import {Observable} from 'rxjs';
 
 
 import { Cliente } from '../../models/cliente.model';
@@ -11,7 +10,7 @@ import { Cliente } from '../../models/cliente.model';
 export class ClientesService {
 
   //private heroesUrl = 'app/heroes';  // URL to web API
-/*
+
   private clientesUrl = 'http://localhost:3000/clientes/nuevo';
 
   constructor (private http: Http) {}
@@ -44,5 +43,5 @@ export class ClientesService {
     console.error(errMsg);
     return Observable.throw(errMsg);
   }
-  */
+
 }
